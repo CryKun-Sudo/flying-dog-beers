8eb28b0532adec61db0b8b00f3e92d2cff1a3751
CryKun-Sudo <71229820+CryKun-Sudo@users.noreply.github.com>
2020-09-14 20:57:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1267,19 +1267,26 @@ def update_output(list_of_contents, artikel_selector,farbe_selector, grobe_selec
 #State('example-artikel-grid','value'),State('example-typeform-grid','value'),State('example-Diametre-grid','value')
 @app.callback([Output('table2','data'),Output('table2','columns')],
 			  [Input('Artikel_Selector',"value"),Input('dropdown','value'),Input('output-confirm', 'children'),Input('output-confirm_mod','children'),Input('output-confirm_del','children')],
-			  [State('output-confirm', 'children'),State('output-confirm_mod','children'),State('output-confirm_del','children'),State('table3','data')])
+			  [State('output-confirm', 'children'),State('output-confirm_mod','children'),State('output-confirm_del','children')])
 def filter_table2(artikel_selector,artikel_values,children,children_mod,children_del,children_state,children_mod_state,children_del_state,table3_state):
 	global reference
-	if artikel_selector == "selected":
-		artikels = artikel_values
+
+	if (children!="") or (children_mod!="") or (children_del!=""):
+
+		if artikel_selector == "selected":
+			artikels = artikel_values
+		else:
+			artikels = reference["Artikel"].unique()
+
+		dff = reference[reference.Artikel.isin(artikels)]
+
+		columns = [{'id': c, 'name': c} for c in dff.columns]
+
+		return dff.to_dict('records'),columns
+
 	else:
-		artikels = reference["Artikel"].unique()
+		return reference.to_dict('records'), [{'id': c, 'name': c} for c in reference.columns]
 
-	dff = reference[reference.Artikel.isin(artikels)]
-
-	columns = [{'id': c, 'name': c} for c in dff.columns]
-
-	return dff.to_dict('records'),columns
 
 
 
