4d3a35fed6260ff9ca25897fd9f0ce4d7386d922
CryKun-Sudo <71229820+CryKun-Sudo@users.noreply.github.com>
2020-09-14 15:54:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1272,7 +1272,8 @@ def update_output(list_of_contents, artikel_selector,farbe_selector, grobe_selec
 			  [Input('Artikel_Selector',"value"),Input('dropdown','value'),Input('output-confirm', 'children'),Input('output-confirm_mod','children'),Input('output-confirm_del','children')],
 			  [State('output-confirm', 'children'),State('output-confirm_mod','children'),State('output-confirm_del','children')])
 def filter_table2(artikel_selector,artikel_values,children,children_mod,children_del,children_state,children_mod_state,children_del_state):
-
+	global reference
+	
 	if artikel_selector == "selected":
 		artikels = artikel_values
 	else:
