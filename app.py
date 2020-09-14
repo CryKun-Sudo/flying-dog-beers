3b9d6e8ae1dd735506a6e075f48994b3366c7d28
CryKun-Sudo <71229820+CryKun-Sudo@users.noreply.github.com>
2020-09-14 14:58:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1268,8 +1268,8 @@ def update_output(list_of_contents, artikel_selector,farbe_selector, grobe_selec
 #State('example-artikel-grid','value'),State('example-typeform-grid','value'),State('example-Diametre-grid','value')
 @app.callback([Output('table2','data'),Output('table2','columns')],
 			  [Input('Artikel_Selector',"value"),Input('dropdown','value'),Input('output-confirm', 'children'),Input('output-confirm_mod','children'),Input('output-confirm_del','children')],
-			  [State('output-confirm', 'children'),State('output-confirm_mod','children'),State('output-confirm_del','children')])
-def filter_table2(artikel_selector,artikel_values,children,children_mod,children_del,children_state,children_mod_state,children_del_state):
+			  [State('output-confirm', 'children'),State('output-confirm_mod','children'),State('output-confirm_del','children'),State('output_save_fdc','children')])
+def filter_table2(artikel_selector,artikel_values,children,children_mod,children_del,children_state,children_mod_state,children_del_state,out_put_save_fdc_state):
 
 
 
