20a9f8686142d53042dffbf2cd2c7fff64752d39
CryKun-Sudo <71229820+CryKun-Sudo@users.noreply.github.com>
2020-09-20 00:50:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,45 @@ UPLOAD_DIRECTORY = "app_uploaded_files"
 LOCAL_DATA = "local_data"
 
 
+def upload_file_git(file_path):
+
+	global update_i
+
+	user = "CryKun-Sudo"
+	password = "Mmaladie123!!!"
+	g = Github(user,password)
+	repo = g.get_user().get_repo('Supply_rachida')
+
+	file_list = [
+
+	file_path
+
+	]
+
+	file_names = [
+	   
+	   file_path
+	]
+
+	commit_message = 'python update %s'%update_i
+	master_ref = repo.get_git_ref('heads/master')
+	master_sha = master_ref.object.sha
+	base_tree = repo.get_git_tree(master_sha)
+	element_list = list()
+	for i, entry in enumerate(file_list):
+		with open(entry) as input_file:
+			data = input_file.read()
+		if entry.endswith('.png'):
+			data = base64.b64encode(data)
+		element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
+		element_list.append(element)
+	tree = repo.create_git_tree(element_list, base_tree)
+	parent = repo.get_git_commit(master_sha)
+	commit = repo.create_git_commit(commit_message, tree, [parent])
+	master_ref.edit(commit.sha)
+
+	update_i+=1
+
 def replace_typeform(element):
 	element = element.strip(' ').lower()
 	if element=="forma":
@@ -1079,7 +1118,7 @@ def prepare_xls(xls_file_path):
 		sheet1[col] = sheet1[col].astype(int)
 	sheet1.to_csv(os.path.join(LOCAL_DATA,"sheet1.csv"),index=False,encoding="utf-8")
 
-	#upload_file_git("local_data/sheet1.csv")
+	upload_file_git("local_data/sheet1.csv")
 
 	return sheet1
 
@@ -1305,7 +1344,9 @@ def filter_table3(data,column,children_upload,children_add,children_mod,children
 
 		sheet2.to_csv(os.path.join(LOCAL_DATA,"sheet2.csv"),index=False,encoding="utf-8")
 
-		#upload_file_git("local_data/sheet2.csv")
+		upload_file_git("local_data/sheet2.csv")
+
+		upload_file_git("local_data/reference.csv")
 
 		columns = []
 
